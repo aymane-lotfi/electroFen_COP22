6f99b1d6368b3dac0b0e0dbd7c5974c11d67d5a9
itachi <hlioui.yassine@gmail.com>
2016-11-07 11:53:18 +0000

    Change resolution to 800x800

--- desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -8,8 +8,8 @@ public class DesktopLauncher {
 	public static void main (String[] arg) {
 		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
 		config.title = "Electrofun - cop22";
-		config.width = 600;
-	    config.height = 480;
+		config.width = 800;
+	    config.height = 800;//480;
 		
 		new LwjglApplication(new ElectroFunCop22(), config);
 	}
