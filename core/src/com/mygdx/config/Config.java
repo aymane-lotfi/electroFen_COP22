619c54b58da7863c89664fc8e8842ab0320bfa60
itachi <hlioui.yassine@gmail.com>
2016-11-08 07:08:02 +0000

    Air-1 : scene + assets

--- core/src/com/mygdx/config/Config.java
+++ core/src/com/mygdx/config/Config.java
@@ -13,11 +13,13 @@ public class Config
     public static String SCENES_PATH = "scenes";
     public static String EVENTS_PATH = "data/events";
 
-    public static String DEFAULT_SCENE = "Eau";//"Terre";//"Eau"; //The Scene's name
+    public static String DEFAULT_SCENE = "Air-1";//"Eau";//"Terre";//"Eau"; //The Scene's name
 
     public static List<String> SCENES_LIST = Arrays.asList(
-            SCENES_PATH+"/Terre.xml",
-            SCENES_PATH+"/Eau.xml"
+            SCENES_PATH+"/Eau.xml",
+            SCENES_PATH+"/Air-1.xml",
+            SCENES_PATH+"/Air-2.xml"
+
     );
 
     public static List<String> EVENTS_LIST = Arrays.asList(
